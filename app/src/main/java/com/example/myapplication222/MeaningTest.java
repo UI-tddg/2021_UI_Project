033bff728c1f45550fd025947372baaf90bae081
Byeonseoyoon <bsy4943@naver.com>
2021-11-16 14:37:04 +0900

    first

---
+++ app/src/main/java/com/example/myapplication222/MeaningTest.java
@@ -0,0 +1,14 @@
+package com.example.myapplication222;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeaningTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meaning_test);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication222/SpellingTest.java
@@ -0,0 +1,14 @@
+package com.example.myapplication222;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpellingTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spelling_test);
+    }
+}
