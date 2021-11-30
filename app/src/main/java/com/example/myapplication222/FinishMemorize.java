c1e1c846f2befe8961d4e64fda0025a84f0ec768
Byeonseoyoon <bsy4943@naver.com>
2021-11-30 15:36:13 +0900

    memorize

---
+++ app/src/main/java/com/example/myapplication222/FinishMemorize.java
@@ -0,0 +1,36 @@
+package com.example.myapplication222;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FinishMemorize extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_memorize);
+
+        Button golist_btn = (Button) findViewById(R.id.go_list_btn);
+        golist_btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ListView.class);
+                startActivity(intent);
+            }
+        });
+
+        Button gotest_btn = (Button) findViewById(R.id.go_test_btn);
+        gotest_btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TestSellect.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/myapplication222/MeanMemorize.java
+++ app/src/main/java/com/example/myapplication222/MeanMemorize.java
@@ -2,6 +2,8 @@ package com.example.myapplication222;
 
         import androidx.annotation.Nullable;
         import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
         import android.os.Bundle;
         import android.view.View;
         import android.widget.Button;
@@ -14,6 +16,9 @@ public class MeanMemorize extends AppCompatActivity implements View.OnClickListe
     TextView showWord;
     ImageButton colorStar;
     ImageButton starButton;
+    ImageButton nextBtn;
+    TextView korText;
+    private int current=0;  //현재 출력되고 있는 배열의 번호
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,10 +29,18 @@ public class MeanMemorize extends AppCompatActivity implements View.OnClickListe
         showWord = (TextView) findViewById(R.id.showWord);
         starButton = (ImageButton) findViewById(R.id.starButton);
         colorStar = (ImageButton) findViewById(R.id.colorStar);
+        nextBtn = (ImageButton)findViewById(R.id.mean_next_btn);
+        korText = (TextView)findViewById(R.id.mean_kor_text);
         touchButton.setOnClickListener(this);
         showWord.setOnClickListener(this);
         starButton.setOnClickListener(this);
         colorStar.setOnClickListener(this);
+        nextBtn.setOnClickListener(this);
+
+        //초기값 세팅
+        korText.setText(ListView.arrayDay[current][2]);
+        showWord.setText(ListView.arrayDay[current][1]);
+        current++;
     }
 
     @Override
@@ -43,6 +56,15 @@ public class MeanMemorize extends AppCompatActivity implements View.OnClickListe
         }
         if (view == colorStar) {
             colorStar.setVisibility(View.INVISIBLE);
+        }
+        if(view == nextBtn){
+            if(current==20) {
+                Intent intent = new Intent(getApplicationContext(), FinishMemorize.class);
+                startActivity(intent);
+            }
+            korText.setText(ListView.arrayDay[current][2]);
+            showWord.setText(ListView.arrayDay[current][1]);
+            current++;
         }
     }
 }
